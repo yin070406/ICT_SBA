add6e4d82f2b822c16901f9902c8f40b12f4dd33
yin070406 <64211270+yin070406@users.noreply.github.com>
2024-09-12 15:38:34 +0800

    Update login.php

--- ICT_SBA/login/login.php
+++ ICT_SBA/login/login.php
@@ -53,19 +53,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                         }
                     }
                 } else{
-                    // Username doesn't exist, display a generic error message
                     $login_err = "Invalid username or password.";
                 }
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
-            // Close statement
             mysqli_stmt_close($stmt);
         }
     }
     
-    // Close connection
     mysqli_close($link);
 }
 ?>
